0de929931c73ff56e400d78223b3d29dfc4cc2c9
Freya Holmér <acegikmo@gmail.com>
2022-11-01 15:35:17 +0100

    acceleration based cube movement

--- Assets/CubeMover.cs
+++ Assets/CubeMover.cs
@@ -3,28 +3,38 @@ using UnityEngine;
 public class CubeMover : MonoBehaviour {
 
 	Vector3 velocity;
-	public float playerSpeed = 1; // meters per second
+	Vector3 acc;
+	public float playerAccMagnitude = 1; // meters per second^2
+	public float drag = 1;
 
 	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
 		Gizmos.DrawRay( transform.position, velocity );
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawRay( transform.position, acc );
 	}
 
 	// every rendered frame
 	public void Update() {
-		velocity = Vector3.zero;
+		acc = Vector3.zero;
 		if( Input.GetKey( KeyCode.W ) )
-			velocity += Vector3.up;
+			acc += Vector3.up;
 		if( Input.GetKey( KeyCode.S ) )
-			velocity += Vector3.down;
+			acc += Vector3.down;
 		if( Input.GetKey( KeyCode.A ) )
-			velocity += Vector3.left;
+			acc += Vector3.left;
 		if( Input.GetKey( KeyCode.D ) )
-			velocity += Vector3.right;
-		velocity = velocity.normalized * playerSpeed;
+			acc += Vector3.right;
+		acc = acc.normalized * playerAccMagnitude;
+		velocity += acc * Time.deltaTime;
 
 		// 1 meter per second
 		float dt = Time.deltaTime;
 		transform.position += velocity * dt;
+	}
+
+	void FixedUpdate() {
+		velocity /= drag; // movement dampening
 	}
 
 }
