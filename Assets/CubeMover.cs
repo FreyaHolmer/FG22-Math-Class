e2bec06cef970ad17bb094b59e83e8d0b652fc97
Freya Holmér <acegikmo@gmail.com>
2022-11-02 10:10:27 +0100

    math function cube movement

--- Assets/CubeMover.cs
+++ Assets/CubeMover.cs
@@ -1,44 +1,24 @@
+using System;
 using UnityEngine;
 
 public class CubeMover : MonoBehaviour {
 
-	Vector3 velocity;
-	Vector3 acc;
-	public float playerAccMagnitude = 1; // meters per second^2
-	public float drag = 1;
+	Vector3 startPos;
+
+	void Awake() => startPos = transform.position;
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.red;
-		Gizmos.DrawRay( transform.position, velocity );
-		Gizmos.color = Color.cyan;
-		Gizmos.DrawRay( transform.position, acc );
+		Gizmos.DrawRay( transform.position, new Vector3( VelocityX( Time.time ), 0, 0 ) );
 	}
+
+	float PositionX( float t ) => -2 * t + t * t;
+
+	float VelocityX( float t ) => -2 + 2 * t;
 
 	// every rendered frame
 	public void Update() {
-		// local function
-		void TestInput( KeyCode key, Vector3 dir ) {
-			if( Input.GetKey( key ) )
-				acc += dir;
-		}
-
-		acc = Vector3.zero;
-		TestInput( KeyCode.W, Vector3.up );
-		TestInput( KeyCode.S, Vector3.down );
-		TestInput( KeyCode.A, Vector3.left );
-		TestInput( KeyCode.D, Vector3.right );
-
-		if( acc != Vector3.zero )
-			acc = acc.normalized * playerAccMagnitude;
-		velocity += acc * Time.deltaTime;
-
-		// 1 meter per second
-		float dt = Time.deltaTime;
-		transform.position += velocity * dt;
-	}
-
-	void FixedUpdate() {
-		velocity /= drag; // movement dampening
+		transform.position = startPos + new Vector3( PositionX( Time.time ), 0, 0 );
 	}
 
 }
